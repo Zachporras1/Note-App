7ff5f4a08ace9132a0126cf4be9147725688d979
Zachary,Porras <ztp567@gmail.com>
2022-11-01 23:14:02 -0400

    initial angular changes

--- note-front/note-frontend/src/app/app.module.ts
+++ note-front/note-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { NotePageComponent } from './component/note-page/note-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NotePageComponent
   ],
   imports: [
     BrowserModule,
